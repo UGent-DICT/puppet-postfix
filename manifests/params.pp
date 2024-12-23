8e9cc2afb59bb86fdb99a9f491ca77a616fe051d
Jan Vansteenkiste <jan.vansteenkiste@ugent.be>
2024-12-23 11:45:28 +0100

    Clean up legacy facts

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,67 +4,67 @@
 #
 class postfix::params
 {
-  $postfix_package = $::operatingsystem ? {
+  $postfix_package = $facts['os']['name'] ? {
     'Solaris'                       => 'CNDpostfix',
     default                         => 'postfix',
   }
 
-  $postfix_ldap_package = $::operatingsystem ? {
+  $postfix_ldap_package = $facts['os']['name'] ? {
     /Ubuntu|Debian/                 => 'postfix-ldap',
     default                         => '',
   }
 
-  $postfix_pcre_package = $::operatingsystem ? {
+  $postfix_pcre_package = $facts['os']['name'] ? {
     /Ubuntu|Debian/                 => 'postfix-pcre',
     default                         => '',
   }
 
-  $postfix_mysql_package = $::operatingsystem ? {
+  $postfix_mysql_package = $facts['os']['name'] ? {
     /Ubuntu|Debian/                 => 'postfix-mysql',
     default                         => '',
   }
 
-  $postfix_cdb_package = $::operatingsystem ? {
+  $postfix_cdb_package = $facts['os']['name'] ? {
     /Ubuntu|Debian/                 => 'postfix-cdb',
     default                         => '',
   }
 
-  $postfix_pgsql_package = $::operatingsystem ? {
+  $postfix_pgsql_package = $facts['os']['name'] ? {
     /Ubuntu|Debian/                 => 'postfix-pgsql',
     default                         => '',
   }
 
-  $postfix_package_provider = $::operatingsystem ? {
+  $postfix_package_provider = $facts['os']['name'] ? {
     'Solaris'                       => 'pkgutil',
     /(?i:CentOS|RedHat|Scientific)/ => 'yum',
     /(?i:Debian|Ubuntu)/            => 'apt',
     default                         => 'postfix',
   }
 
-  $mailx_package = $::operatingsystem ? {
+  $mailx_package = $facts['os']['name'] ? {
     'Solaris'                       => undef,
     /(?i:CentOS|RedHat|Scientific)/ => 'mailx',
     /(?i:Debian|Ubuntu)/            => 'bsd-mailx',
   }
 
-  $service_name = $::operatingsystem ? {
+  $service_name = $facts['os']['name'] ? {
     default => 'postfix'
   }
 
-  $daemon_uid = $::operatingsystem ? {
+  $daemon_uid = $facts['os']['name'] ? {
     default                         => 'postfix'
   }
 
-  $daemon_gid = $::operatingsystem ? {
+  $daemon_gid = $facts['os']['name'] ? {
     'Solaris'                       => 'other',
     default                         => 'postfix'
   }
 
-  $data_directory = $::operatingsystem ? {
+  $data_directory = $facts['os']['name'] ? {
     default                        => '/var/lib/postfix',
   }
 
-  $aliases_database = $::operatingsystem ? {
+  $aliases_database = $facts['os']['name'] ? {
     'Solaris'                       => '/etc/mail/aliases',
     default                         => '/etc/aliases'
   }
